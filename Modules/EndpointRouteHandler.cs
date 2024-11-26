d07128812662bca8522b11b07b1545e375770742
GeekGuste <manouauguste7@gmail.com>
2024-11-26 19:46:56 +0100

    Add project files.

---
+++ Modules/EndpointRouteHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApi.Modules;
+
+public static class EndpointRouteHandler
+{
+    public static WebApplication MapEndpoints(this WebApplication app)
+    {
+        app.MapGroup("/form").RegisterFormEndpoints();
+        return app;
+    }
+}
---
+++ Modules/FormEndpoints.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApi.Modules;
+
+public static class FormEndpoints
+{
+    public static RouteGroupBuilder RegisterFormEndpoints(this RouteGroupBuilder app)
+    {
+
+        app.MapGet("/", () => "Hello World");
+
+        app.MapPost("/", () => "Hello World");
+
+        app.MapPut("/{formId}", (int formId) => "Hello World!");
+
+        return app;
+    }
+}
